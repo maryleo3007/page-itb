1a082c1a3956a1a85f0e4f99593de6ec4be7ba64
maryleo3007 <maryleo.3007@gmail.com>
2018-10-01 00:25:41 -0500

    add project

--- sb-admin.js
+++ sb-admin.js
@@ -52,3 +52,59 @@ setTimeout( () => goToSlide(slideElements, 1), 6000 );
 // }
 
 // let progressInterval = setInterval(progress,20);
+
+AOS.init();
+
+
+AOS.init({
+  
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+
+  
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
+
+$(window).scroll(function() {
+	var $height = $(window).scrollTop();
+  if($height > 709) {
+    $('#mainNav').addClass('active');
+    
+	} else {
+    $('#mainNav').removeClass('active');
+  }
+  if($height < 500) {
+    $('.home-cls').addClass('active');
+    
+	} else {
+    $('.home-cls').removeClass('active');
+  }
+  if($height > 500 && $height < 1116) {
+    $('.service-cls').addClass('active');
+    
+	} else {
+    $('.service-cls').removeClass('active');
+  }
+  if($height > 1116 &&  $height < 2176) {
+    $('.nosotros-cls').addClass('active');
+    
+	} else {
+    $('.nosotros-cls').removeClass('active');
+  }
+  if($height > 2176) {
+    $('.contact-cls').addClass('active');
+    
+	} else {
+    $('.contact-cls').removeClass('active');
+  }
+  
+});
