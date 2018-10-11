ddb8ca0c8d0680150c796d2361729791a132a0ee
maryleo3007 <maryleo.3007@gmail.com>
2018-10-10 23:48:57 -0500

    add bootstrap material

--- sb-siscom.js
+++ sb-siscom.js
@@ -63,27 +63,87 @@ $(window).scroll(function() {
     }
 });
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    // margin:10,
-    responsiveClass:true,
-    autoplay: true,
-    dots: false,
-    nav:false,
-    animateIn: true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false
-        },
-        600:{
-            items:2,
-            nav:false
-        },
-        1000:{
-            items:1,
-            nav:false,
-            loop:true
+$('#btn-funcionalidades').on('click', function (e) {
+    e.preventDefault();
+    $('#btn-como-funciona').removeClass('activo');
+    $(this).addClass('activo');
+    $('#content-como-funciona').addClass('none');
+    
+    $('#content-funcionalidades').removeClass('none');
+});
+
+$('#btn-como-funciona').on('click', function (e) {
+    e.preventDefault();
+    $('#btn-funcionalidades').removeClass('activo');
+    $(this).addClass('activo');
+    $('#content-como-funciona').removeClass('none');
+    $('#content-funcionalidades').addClass('none');
+
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        // margin:10,
+        responsiveClass:true,
+        autoplay: true,
+        dots: true,
+        nav:true,
+        animateIn: true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:1,
+                nav:false,
+                loop:true
+            }
         }
-    }
-})
+    });
+
+});
+
+
+
+
+$(document).ready(function () {
+    
+    $('#btn-funcionalidades').addClass('active');
+    // $('#fullpage').fullpage({
+    //     sectionColor: ['#cccccc','#fffffff','#cccccc','#fffffff','#cccccc','#fffffff'],
+    //     easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)',
+    //     // loopTop: true,
+    //     // loopBottom: true,
+    //     // navigation: true,
+    //     // navigationPosition: true,
+    //     // navigationTooltips: ['Section 1', 'Section 2','Section 3','Section 4','Section 5','Section 6'],
+    //     // afterLoad: function (anchor, index, direction) {
+            
+    //     // },
+    //     // afterLoad: function (index, netIndex, direction) {
+            
+    //     // }
+
+    // });  
+
+    $(".main").onepage_scroll({
+        sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+        easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+                                         // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+        animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+        pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+        updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+        // beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
+        // afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
+        loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+        keyboard: true,                  // You can activate the keyboard controls
+        responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+                                         // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+                                         // the browser's width is less than 600, the fallback will kick in.
+        direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
+     });
+});
